c997f40650192939a15f35c8541b95b403c056d6
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-02-01 20:52:46 +0100

    [chor] Change Commands controler class to module
    Now we don't have reimporting files, when controler was reinitialized.
    It saves RAM and improve readability

--- commands/say.ts
+++ commands/say.ts
@@ -17,7 +17,7 @@
     =======================================         
     @project: Paradise BOT;
     @author: WH1T3R0S3 SQUAD (thenbhd#0001);
-    @filename: commands/say.js;
+    @filename: commands/say.ts;
 
     All rights are reserved.
     =======================================
@@ -26,17 +26,16 @@
 /*
     How to do the command?
 
-    module.exports = {
-        name: "name", <-- Command name
-        callback: (socket, stats, args) => { <-- Command code
+    export const name = (socket, stats, args) => { <-- Command code
             <!- THERE PUT THE CODE OF COMMAND -!>
         }
-    }
+
+    and add to `commands/index.ts`
+
+    export * from './name';
 */
 
-module.exports = {
-    name: "say",
-    callback: (socket, stats, args) => {
-        return socket.send(JSON.stringify([0, args.join(" ")]));
-    }
-};
+/**
+ * Sand message to in-game chat.
+ */
+export const say = (socket, stats, args) => socket.send(JSON.stringify([0, args.join(" ")]));
